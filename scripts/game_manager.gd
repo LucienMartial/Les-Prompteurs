232d4bfa606f1c74bcd58a5f4907989344b0cbfb
Flavien <flavhirtz@gmail.com>
2024-12-01 00:42:05 +0100

    fix: draggable mirrors on occupied tile

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,17 +5,17 @@ var objects = {}
 
 const GRID_SIZE: int = 16
 
-func handle_dropped_object(global_pos: Vector2):
+func handle_dropped_object(global_pos: Vector2) -> bool:
 	var snapped_pos = snap_to_grid(global_pos)
 	
 	if(str(snapped_pos) in objects):
-		print("Case occupée")
-		return
+		return false
 	
 	var instance = mirror.instantiate()
 	instance.position = snapped_pos
 	get_parent().add_child(instance)
 	objects[str(snapped_pos)] = instance
+	return true
 
 func snap_to_grid(position: Vector2):
 	return Vector2(
--- scripts/inventory_button.gd
+++ scripts/inventory_button.gd
@@ -3,9 +3,11 @@ extends TextureButton
 var dragging := false
 var click_offset := Vector2.ZERO
 
+var origin_position: Vector2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	z_index = 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -15,10 +17,15 @@ func _process(_delta: float) -> void:
 func _on_button_down() -> void:
 	dragging = true
 	click_offset = get_global_mouse_position() - position
+	origin_position = position
 
 func _on_button_up() -> void:
 	if dragging:
-		%GameManager.handle_dropped_object(global_position)
-		dragging = false
-		click_offset = Vector2.ZERO
-		queue_free()
+		var success: bool = %GameManager.handle_dropped_object(global_position)
+		if(success):
+			queue_free()
+		else:
+			dragging = false
+			click_offset = Vector2.ZERO
+			position = origin_position
+			
